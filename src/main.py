0e4e37cbd991cc8e128910ea09a9f05d35a2a6d6
Yurii Motov <yurii.motov.monte@gmail.com>
2025-01-18 21:34:16 +0100

    Create init method. Tests for init

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
+import typing
 from typing import Any, TypeVar, cast
 
 
 class MyModelField:
-
     def __init__(self, field_name, field_annotation):
         self._name = field_name
         self._field_annotation = field_annotation
@@ -17,29 +17,55 @@ class MyModelField:
     def __set__(self, instance, value):
         attr_name = f"_{self._name}"
         if not isinstance(value, self._field_annotation):
-            msg = f"Invalid value. Expected type: {self._field_annotation.__name__}"
+            msg = (
+                "Invalid value. "
+                f"Expected type {self._field_annotation.__name__}, "
+                f"got {type(value)}. Input: {value}"
+            )
             raise ValueError(msg)  # noqa: TRY004
         instance.__dict__[attr_name] = value
 
 
+class ValidationError(Exception):
+    def __init__(self, msg: str):
+        self.msg = msg
+
+
+@typing.dataclass_transform(kw_only_default=True)
 class Meta(type):
     def __new__(cls, name, bases, dct: dict[str, Any]):
-
         fields_annotations = cast(dict[str, Any], dct.get("__annotations__"))
 
         obj = super().__new__(cls, name, bases, dct)
 
+        defaults: dict[str, Any] = {}
         if fields_annotations:
             for field_name, field_type in fields_annotations.items():
                 setattr(obj, field_name, MyModelField(field_name, field_type))
+                if field_name in dct:
+                    defaults[field_name] = dct[field_name]
+
+        def __init__(self: "ModelBase", *args, **kwargs):
+            if len(args):
+                msg = "Can only accept keyword arguments"
+                raise RuntimeError(msg)
+            if fields_annotations:
+                for field_name, field_type in fields_annotations.items():  # noqa: B007
+                    if (field_name not in kwargs) and (field_name not in defaults):
+                        msg = f"No value for required field was provided: {field_name}"
+                        raise ValidationError(msg)
+                    val = kwargs.get(field_name, defaults.get(field_name))
+                    self.__setattr__(field_name, val)
+
+        obj.__init__ = __init__
 
         return obj
 
 
 T = TypeVar("T", bound="ModelBase")
 
-class ModelBase(metaclass=Meta):
 
+class ModelBase(metaclass=Meta):
     @classmethod
     def validate(cls: type[T], data: dict[str, Any], partial: bool = False) -> T:
         if partial is False:
@@ -49,4 +75,3 @@ class ModelBase(metaclass=Meta):
         for field_name, field_val in data.items():
             setattr(obj, field_name, field_val)
         return obj
-
---
+++ tests/test_init_created.py
@@ -0,0 +1,52 @@
+import pytest
+
+from src.main import ModelBase, ValidationError
+
+
+class MyClass(ModelBase):
+    int_field: int
+    str_field: str
+    field_with_default: str = "default value"
+
+
+def test__init__success():
+    """
+    Create instance using init method, provide all values.
+    """
+    TEST_INT_VALUE = 123
+    TEST_STR_VALUE = "asd"
+    TEST_STR_VALUE_2 = "new value"
+
+    inst = MyClass(
+        int_field=TEST_INT_VALUE,
+        str_field=TEST_STR_VALUE,
+        field_with_default=TEST_STR_VALUE_2,
+    )
+
+    assert inst.int_field == TEST_INT_VALUE
+    assert inst.str_field == TEST_STR_VALUE
+    assert inst.field_with_default == TEST_STR_VALUE_2
+
+
+def test__init_only_required__success():
+    """
+    Create instance using init method, provide only required values.
+    """
+    TEST_INT_VALUE = 123
+    TEST_STR_VALUE = "asd"
+
+    inst = MyClass(
+        int_field=TEST_INT_VALUE,
+        str_field=TEST_STR_VALUE,
+    )
+
+    assert inst.field_with_default == "default value"
+
+
+def test__init__required_field_not_provided():
+    """
+    Create instance not providing required field throws ValidationError.
+    """
+
+    with pytest.raises(ValidationError):
+        MyClass()
